18b57e0b18f11975e38878acecf07a0e32ee541f
林コウイ <RyanHoward@hayashikoui-no-MacBook-Pro.local>
2014-10-25 14:49:21 +0900

    Initial Commit

---
+++ ClassEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClassEx
+//
+//  Created by 林コウイ on 2014/10/25.
+//  Copyright (c) 2014年 ryanhoward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClassEx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassEx
+//
+//  Created by 林コウイ on 2014/10/25.
+//  Copyright (c) 2014年 ryanhoward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
